f7853c67599c069c3f660c1b66bb2f928a5d8355
Jabelic <hibiki990523@gmail.com>
2022-05-06 19:42:49 +0900

    ADD: bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
     # https://qiita.com/jnchito/items/ea7832df6f64a9034872
     # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     VALID_EMAIL_REGEX = /\A[\w+\-]+[\.]*[\w+\-]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates(:name, presence: true ,length: { maximum: 50 })
-    validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-    #  uniqueness: { case_sensitive: false } 大文字小文字を区別してユニークであること
+    validates :name, presence: true ,length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false } #  uniqueness: { case_sensitive: false } 大文字小文字を区別してユニークであること
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
