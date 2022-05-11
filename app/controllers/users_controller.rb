0d273ea56a3ceccef62bdf57f63c90360dd128bb
Jabelic <hibiki990523@gmail.com>
2022-05-12 02:26:24 +0900

    ADD: user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+  def show
+    # local変数は    hoge
+    # instance変数は @hoge
+    # grobal変数は   @@hoge
+    @user = User.find(params[:id])
+    # debugger
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+    # 引数で与えられたユーザーのGravatar画像を返す
+    def gravatar_for(user, options = { size: 80 })
+        size = options[:size]
+        # MD5でemailのdowncaseをhash化
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        # gravatarで生成される画像のurl
+        # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end # 引数で与えられたユーザーのGravatar画像を返す
 end
