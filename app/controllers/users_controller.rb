8b49b411ae05493373e50fa9cb73afd4771afa30
Jabelic <hibiki990523@gmail.com>
2022-05-14 17:12:58 +0900

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,15 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 public
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     # local変数は    hoge
     # instance変数は @hoge
@@ -26,5 +34,16 @@ public
   end
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 end
