e37f109d4b103a8f7edc0c368cea9c83da660db6
Jabelic <hibiki990523@gmail.com>
2022-05-24 23:53:00 +0900

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,38 @@
 class UsersController < ApplicationController
 private
   def user_params
+    # admin権限は変更できないようにしている。
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  # ログイン済みユーザーかどうか確認
+  # # ログイン済みユーザーかどうか確認
   def logged_in_user
     unless logged_in?
+      store_location # アクセスしようとしたpathを覚えておく
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
   end
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 public
-  before_action :logged_in_user, only: [:edit, :update]
+  # before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy] # login user は
+  before_action :correct_user,   only: [:edit, :update] # 正しいユーザーかどうか
+  before_action :admin_user,     only: :destroy
+  # indexはログインユーザーのみアクセス可能
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page])
+  end
   def show
     # local変数は    hoge
     # instance変数は @hoge
@@ -45,5 +66,12 @@ public
     else
       render 'edit'
     end
+  end
+  
+  # Delete
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 end
