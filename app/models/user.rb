e571b7892700c914963d1e78cbefeff83b00ab36
Jabelic <hibiki990523@gmail.com>
2022-05-28 11:25:00 +0900

    hoftix: authenticated method

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,10 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
-    # 渡されたトークンがダイジェストと一致したらtrueを返す
-    def authenticated?(remember_token)
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
+    # # 渡されたトークンがダイジェストと一致したらtrueを返す
+    # def authenticated?(remember_toke n)
+    #     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    # end
 
     # 渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticated?(remember_token)
